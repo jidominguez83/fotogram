e0b2cacdc16809033997c09e95d95378485ed94e
jidominguez83 <domingueztorres.ji@gmail.com>
2022-04-19 13:48:26 -0500

    Se modificó el archivo database.sql y algunos íconos

--- database.sql
+++ database.sql
@@ -16,10 +16,6 @@ CREATE TABLE IF NOT EXISTS users(
     CONSTRAINT pk_users PRIMARY KEY(id)
 )ENGINE=InnoDB;
 
-INSERT INTO users VALUES(NULL, 'user', 'Jesús Iván', 'Domínguez Torres', 'jidominguez', 'domingueztorres.ji@gmail.com', '123456', NULL, CURTIME(), CURTIME(), NULL);
-INSERT INTO users VALUES(NULL, 'user', 'Anaclara María del Mar', 'Jonguitud Infante', 'anaclara', 'anaclara_jomguitud@hotmail.com', '123456', NULL, CURTIME(), CURTIME(), NULL);
-INSERT INTO users VALUES(NULL, 'user', 'Zaira Lucía', 'Álvarez Suárez', 'zaira', 'zairalu@gmail.com', '123456', NULL, CURTIME(), CURTIME(), NULL);
-
 CREATE TABLE IF NOT EXISTS images(
     id          int(11) auto_increment not null,
     user_id     int(11),
@@ -30,16 +26,6 @@ CREATE TABLE IF NOT EXISTS images(
     CONSTRAINT pk_images PRIMARY KEY(id),
     CONSTRAINT fk_images_users FOREIGN KEY(user_id) REFERENCES users(id)
 )ENGINE=InnoDB;
-
-INSERT INTO images VALUES(NULL, 1, 'test.jpg', 'Cogiendo con Anaclara', CURTIME(), CURTIME());
-INSERT INTO images VALUES(NULL, 1, 'playa.jpg', 'Cogiendo con Zaira', CURTIME(), CURTIME());
-INSERT INTO images VALUES(NULL, 1, 'lago.jpg', 'Cogiendo con Zaira y Anaclara', CURTIME(), CURTIME());
-INSERT INTO images VALUES(NULL, 2, 'conivan.jpg', 'Cogiendo con Iván', CURTIME(), CURTIME());
-INSERT INTO images VALUES(NULL, 2, 'conzaira.jpg', 'Cogiendo con Zaira', CURTIME(), CURTIME());
-INSERT INTO images VALUES(NULL, 2, 'trioconivanyzaira.jpg', 'Cogiendo con Iván y Zaira', CURTIME(), CURTIME());
-INSERT INTO images VALUES(NULL, 3, 'follandoconivan.jpg', 'Cogiendo con Iván', CURTIME(), CURTIME());
-INSERT INTO images VALUES(NULL, 3, 'conanaclara.jpg', 'Cogiendo con Anaclara', CURTIME(), CURTIME());
-INSERT INTO images VALUES(NULL, 3, 'trioconivanyanaclara.jpg', 'Cogiendo con Iván y Anaclara', CURTIME(), CURTIME());
 
 CREATE TABLE IF NOT EXISTS comments(
     id          int(11) auto_increment not null,
@@ -53,10 +39,6 @@ CREATE TABLE IF NOT EXISTS comments(
     CONSTRAINT fk_comments_images FOREIGN KEY(image_id) REFERENCES images(id)
 )ENGINE=InnoDB;
 
-INSERT INTO comments VALUES(NULL, 1, 3, 'Que ricas se ven chupandome la verga, Zaira y Anaclara!', CURTIME(), CURTIME());
-INSERT INTO comments VALUES(NULL, 2, 6, 'Que rico nos la metes Iván!', CURTIME(), CURTIME());
-INSERT INTO comments VALUES(NULL, 3, 9, 'Nos comimos tu leche deliciosa! y que ricas tetas tienes Anaclara!', CURTIME(), CURTIME());
-
 CREATE TABLE IF NOT EXISTS likes(
     id          int(11) auto_increment not null,
     user_id     int(11),
@@ -67,10 +49,3 @@ CREATE TABLE IF NOT EXISTS likes(
     CONSTRAINT fk_likes_users FOREIGN KEY(user_id) REFERENCES users(id),
     CONSTRAINT fk_likes_images FOREIGN KEY(image_id) REFERENCES images(id)
 )ENGINE=InnoDB;
-
-INSERT INTO likes VALUES(NULL, 1, 2, CURTIME(), CURTIME());
-INSERT INTO likes VALUES(NULL, 2, 7, CURTIME(), CURTIME());
-INSERT INTO likes VALUES(NULL, 3, 8, CURTIME(), CURTIME());
-INSERT INTO likes VALUES(NULL, 1, 4, CURTIME(), CURTIME());
-INSERT INTO likes VALUES(NULL, 2, 1, CURTIME(), CURTIME());
-INSERT INTO likes VALUES(NULL, 3, 9, CURTIME(), CURTIME());
